6fed2dcee0f11f5f4fbfd221471c0912b14e8030
wenxuan-pan <panwenxuan42@gmail.com>
2024-01-08 16:08:23 +1100

    chap 3 done

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-const styleguide = require('@vercel/style-guide/prettier')
-
-module.exports = {
-  ...styleguide,
-  semi: false,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
-}
